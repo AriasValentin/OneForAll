6dd510813a035ad9a8e59d3aafea470af88844d7
Valentin Arias <ariasvalentin1422@gmail.com>
2023-10-16 18:39:48 -0300

    mejorando componentes y paletas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioLoginComponent } from './components/formulario-login/formulario-login.component';
-import { BotonesLoginComponent } from './components/botones-login/botones-login.component';
+
 import { FooterComponent } from './components/footer/footer.component';
 import { NavegadorComponent } from './components/navegador/navegador.component';
 
@@ -12,7 +12,6 @@ import { NavegadorComponent } from './components/navegador/navegador.component';
   declarations: [
     AppComponent,
     FormularioLoginComponent,
-    BotonesLoginComponent,
     FooterComponent,
     NavegadorComponent
   ],
