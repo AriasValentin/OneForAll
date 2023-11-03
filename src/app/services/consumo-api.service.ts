9a504d4c8c2015baaf7d6ef4cfb6cc20a45b83b6
Valentin Arias <ariasvalentin1422@gmail.com>
2023-11-03 13:37:34 -0300

    cambios tarjetas hay errores

--- src/app/services/consumo-api.service.ts
+++ src/app/services/consumo-api.service.ts
@@ -26,7 +26,7 @@ export class ConsumoApiService {
   conseguirDatos(): Carta[] {
     let cartas: Carta[] = [];
   
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 800; i++) {
       this.getCharacter(i).subscribe(data => {
         let carta: Carta = {
           id: data.id,
